a7d308b595a5260d0a06e30764bf0f44b267e3a2
Mohammad Shaker <mohamed.shaker@itworx.com>
2019-07-20 21:49:00 +0200

    Initial Commit

---
+++ Translations Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Translations Demo
+//
+//  Created by Mohammad Shaker on 7/20/19.
+//  Copyright © 2019 Mohammad Shaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
